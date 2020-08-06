c1d99c20ddaf687e0c19f9eeb220185c28ed1192
Lubota Ngola Joao <lubota4@gmail.com>
2020-08-06 15:01:52 -0300

    .

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/projects", (request, response) => {
-  return response.json({ message: "Hello World" });
+  return response.json({ message: "Salut le monde!" });
 });
 
 app.listen(3333);
