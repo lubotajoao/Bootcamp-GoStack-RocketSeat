38cd7a4da55a634e32defd3d001d975b742753cb
Lubota Ngola Joao <lubota4@gmail.com>
2020-08-06 15:25:12 -0300

    Configurando Nodemon

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,4 +6,6 @@ app.get("/projects", (request, response) => {
   return response.json({ message: "Salut le monde2!" });
 });
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log("🚀️ Back-end Started!");
+});
