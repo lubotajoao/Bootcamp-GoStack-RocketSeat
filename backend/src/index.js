302e6d4ac6bdf7078a10ad5779aa9350ed477957
Lubota Ngola Joao <lubota4@gmail.com>
2020-08-07 09:37:15 -0300

    Parameteres Types

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 /**
  * Métodos HTTP:
  *
@@ -11,15 +13,36 @@ const app = express();
  * DELETE: Deletar uma informação no back-end
  */
 
+/**
+ * Tipos de Parâmetros: formas que o client tem de enviar uma requisição
+ *
+ * Query Params: Utilizado principalmente para filtros e paginação.
+ * Route Params: Identificar recursos na hora de atualizar ou deletar
+ * Request Body: Conteúdo na hora de criar ou editar um recurso (feito através de JSON)
+ */
+
 app.get("/projects", (request, response) => {
+  const { title, owner } = request.query;
+  console.log(title);
+  console.log(owner);
+
   return response.json(["Projeto1", "Projeto2"]);
 });
 
 app.post("/projects", (request, response) => {
+  const { title, owner } = request.body;
+
+  console.log(title);
+  console.log(owner);
+
   return response.json(["Projeto1", "Projeto 2", "Projeto 3"]);
 });
 
 app.put("/projects/:id", (request, response) => {
+  const { id } = request.params;
+
+  console.log(id);
+
   return response.json(["Projeto4", "Projeto 2", "Projeto 3"]);
 });
 
