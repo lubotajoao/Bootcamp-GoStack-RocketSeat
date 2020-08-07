097c7c4d1dd018de012f0fc37451d26b56cd1bdb
Lubota Ngola Joao <lubota4@gmail.com>
2020-08-07 08:54:21 -0300

    HTTP Methods: GET, POST,PUT/PATCH & DELETE

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,29 @@ const express = require("express");
 
 const app = express();
 
+/**
+ * Métodos HTTP:
+ *
+ * GET: Buscar informações no back-end
+ * POST: Criar uma informação no back-end
+ * PUT/PATCH: Alterar uma informação no back-end (PaTCH é para apenas uma informação)
+ * DELETE: Deletar uma informação no back-end
+ */
+
 app.get("/projects", (request, response) => {
-  return response.json({ message: "Salut le monde2!" });
+  return response.json(["Projeto1", "Projeto2"]);
+});
+
+app.post("/projects", (request, response) => {
+  return response.json(["Projeto1", "Projeto 2", "Projeto 3"]);
+});
+
+app.put("/projects/:id", (request, response) => {
+  return response.json(["Projeto4", "Projeto 2", "Projeto 3"]);
+});
+
+app.delete("/projects/:id", (request, response) => {
+  return response.json(["Projeto 2", "Projeto 3"]);
 });
 
 app.listen(3333, () => {
