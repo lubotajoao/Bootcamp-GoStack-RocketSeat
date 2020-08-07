9e5c0e1a860f1a115b598a2f64a605eaccb845dd
Lubota Ngola Joao <lubota4@gmail.com>
2020-08-07 19:30:28 -0300

    Functional Application: Routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+// const { uuid } = require("uuidv4");
+
+const { v4: uuid_v4 } = require("uuid");
 
 const app = express();
 
@@ -18,36 +21,61 @@ app.use(express.json());
  *
  * Query Params: Utilizado principalmente para filtros e paginação.
  * Route Params: Identificar recursos na hora de atualizar ou deletar
- * Request Body: Conteúdo na hora de criar ou editar um recurso (feito através de JSON)
+ * Request Body: Conteúdo na hora de criar ou editar um recurso (através de JSON)
  */
 
-app.get("/projects", (request, response) => {
-  const { title, owner } = request.query;
-  console.log(title);
-  console.log(owner);
+const projects = [];
 
-  return response.json(["Projeto1", "Projeto2"]);
+app.get("/projects", (request, response) => {
+  const { title } = request.query;
+
+  const results = title
+    ? projects.filter((project) => project.title.includes(title))
+    : projects;
+
+  return response.json(results);
 });
 
 app.post("/projects", (request, response) => {
   const { title, owner } = request.body;
 
-  console.log(title);
-  console.log(owner);
+  const project = { id: uuid_v4(), title, owner };
 
-  return response.json(["Projeto1", "Projeto 2", "Projeto 3"]);
+  projects.push(project);
+
+  return response.json(project);
 });
 
 app.put("/projects/:id", (request, response) => {
   const { id } = request.params;
 
-  console.log(id);
+  const { title, owner } = request.body;
 
-  return response.json(["Projeto4", "Projeto 2", "Projeto 3"]);
+  const projectIndex = projects.findIndex((project) => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: "Project not found!" });
+  }
+
+  const project = { id, title, owner };
+
+  projects[projectIndex] = project;
+
+  return response.json(project);
 });
 
 app.delete("/projects/:id", (request, response) => {
-  return response.json(["Projeto 2", "Projeto 3"]);
+  const { id } = request.params;
+
+  const projectIndex = projects.findIndex((project) => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: "Project not found!" });
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.listen(3333, () => {
