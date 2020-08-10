36cddc8aaac39110314821573de646162d5866cb
Lubota Ngola Joao <lubota4@gmail.com>
2020-08-09 21:47:14 -0300

    React Native Concepts: ReactJS vs React Native

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,6 +1,35 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text, StyleSheet, StatusBar} from 'react-native';
+
+// Nenhum elemento () tem valor semantico (significado)
+// Nenhum elemento possue estilização própria
+// Todos componentes possuem por padrão "display: flex"
+
+// View: representa -> div, footer, header, main, aside, section, etc
+// Text: p, span, strong, h1, h2, h3
 
 export default function App() {
-  return <View></View>;
+  return (
+    <>
+      <StatusBar barStyle="default" backgroundColor="#7159C1" />
+      <View style={styles.container}>
+        <Text style={styles.title}>Salut le monde!!!</Text>
+      </View>
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159C1',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  title: {
+    color: '#FFF',
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+});
