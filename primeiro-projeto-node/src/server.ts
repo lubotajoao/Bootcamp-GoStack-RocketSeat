8e87c0d0a9041bf9e6152c19f1292c0c868a2ef3
Lubota Ngola Joao <lubota4@gmail.com>
2020-08-13 23:45:16 -0300

    First Project With Node.JS: Debug Config

--- primeiro-projeto-node/src/server.ts
+++ primeiro-projeto-node/src/server.ts
@@ -1,11 +1,15 @@
-import express from "express";
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Salut monde programmation!!!!!!!" });
+app.use(express.json());
+app.use(routes);
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Salut monde program!!!!!!!' });
 });
 
 app.listen(3333, () => {
-  console.log("🏁️ Server started on port 3333!");
+  console.log('🏁️ Server started on port 3333!');
 });
