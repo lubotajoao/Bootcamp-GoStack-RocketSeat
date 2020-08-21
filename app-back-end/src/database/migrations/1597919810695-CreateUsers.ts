4a3c5614576d2b653c1ccbebb1f96bee15b303ed
Lubota Ngola Joao <lubota4@gmail.com>
2020-08-21 06:06:16 -0300

    Back-end do App: Set Relationship in Models

--- app-back-end/src/database/migrations/1597919810695-CreateUsers.ts
+++ app-back-end/src/database/migrations/1597919810695-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1597919810695 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
