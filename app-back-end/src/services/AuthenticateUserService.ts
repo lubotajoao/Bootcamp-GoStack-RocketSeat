30ace0d471ba2992dfffec69b25ae519a25bb5f0
Lubota Ngola Joao <lubota4@gmail.com>
2020-08-22 19:38:12 -0300

    Back-end do App: Generating JWT Token

--- app-back-end/src/services/AuthenticateUserService.ts
+++ app-back-end/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 export default class AuthenticateUserService {
@@ -28,8 +30,14 @@ export default class AuthenticateUserService {
       throw new Error('Incorrect email/password combination');
     }
 
+    const token = sign({}, '0a71c4333cf8f11d48537bba838a40d7', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     return {
       user,
+      token,
     };
   }
 }
