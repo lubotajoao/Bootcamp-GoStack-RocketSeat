245ccb1beaed76789a2f8e0dad1cba55b2ead1cd
Lubota Ngola Joao <lubota4@gmail.com>
2020-08-26 09:45:51 -0300

    Back-end do App: Serving static Files

--- app-back-end/src/server.ts
+++ app-back-end/src/server.ts
@@ -2,17 +2,15 @@ import 'reflect-metadata';
 
 import express from 'express';
 import routes from './routes';
+import uploadConfig from './config/upload';
 
 import './database';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
-
-app.get('/', (request, response) => {
-  return response.json({ message: 'Salut monde program!!!!!!!' });
-});
 
 app.listen(3333, () => {
   console.log('🏁️ Server started on port 3333!');
