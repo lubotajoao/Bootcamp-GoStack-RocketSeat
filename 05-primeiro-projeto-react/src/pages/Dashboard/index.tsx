422d1107887d18ec93f23e77b220f68fab56ee92
Lubota Ngola Joao <lubota4@gmail.com>
2020-09-03 06:55:53 -0300

    1ST Project With React: Dashboard Styles

--- 05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ 05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,9 +1,76 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import Title from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no GitHub</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="GitHub Explorer" />
+      <Title>Explore repositórios no GitHub</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/14189040?s=460&u=cb9a26a2a079a66be53aa18db3ddad1b7d32bf62&v=4"
+            alt="Lubota Ngola Joao"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/14189040?s=460&u=cb9a26a2a079a66be53aa18db3ddad1b7d32bf62&v=4"
+            alt="Lubota Ngola Joao"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/14189040?s=460&u=cb9a26a2a079a66be53aa18db3ddad1b7d32bf62&v=4"
+            alt="Lubota Ngola Joao"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/14189040?s=460&u=cb9a26a2a079a66be53aa18db3ddad1b7d32bf62&v=4"
+            alt="Lubota Ngola Joao"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
