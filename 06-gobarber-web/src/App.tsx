1f6daa081e76ca1c758f8bb1b900b7f9ea8c4ed2
Lubota Ngola Joao <lubota4@gmail.com>
2020-09-18 07:45:32 -0300

    GoBarber Web: Pages Structure | Global Styles

--- 06-gobarber-web/src/App.tsx
+++ 06-gobarber-web/src/App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
 
-const App: React.FC = () => <h1>Salut le monde...</h1>
+const App: React.FC = () => (
+  <>
+    <h1>Salut le monde...</h1>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
