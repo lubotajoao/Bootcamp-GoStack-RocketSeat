0250536170e4a7fc5f5d70acad4f6d565d0bbc80
Lubota Ngola Joao <lubota4@gmail.com>
2020-09-23 07:35:31 -0300

    GoBarber Web: Pages Structure | SignIn Validation

--- 06-gobarber-web/src/pages/SignUp/index.tsx
+++ 06-gobarber-web/src/pages/SignUp/index.tsx
@@ -24,7 +24,9 @@ const SignUp: React.FC = () => {
 
       const schema = Yup.object().shape({
         name: Yup.string().required('Nome obrigatório'),
-        email: Yup.string().required('E-mail obrigatório').email(),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
         password: Yup.string().min(6, 'No mínimo 6 dígitos'),
       });
 
