c3c870ba416af8012b48b6d1b8ab4ce3fe53dfdd
Lubota Ngola Joao <lubota4@gmail.com>
2020-09-23 22:22:45 -0300

    Back-end do App: Enable CORS

--- 04-iniciando-back-end/src/server.ts
+++ 04-iniciando-back-end/src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import uploadConfig from './config/upload';
@@ -10,27 +11,26 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
-app.use(
-  (err: Error, request: Request, response: Response, _: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        status: 'error',
-        message: err.message,
-      });
-    }
-
-    console.log(err);
-
-    return response.status(500).json({
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
       status: 'error',
-      message: 'Internal server error',
+      message: err.message,
     });
-  },
-);
+  }
+
+  console.log(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => {
   console.log('🏁️ Server started on port 3333!');
