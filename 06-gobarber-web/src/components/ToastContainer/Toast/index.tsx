9a7939f467b6ed3f3d18416972a12a8287d985a3
Lubota Ngola Joao <lubota4@gmail.com>
2020-09-26 22:40:38 -0300

    GoBarber Web: Toast Msgs | Annimating Toasts

--- 06-gobarber-web/src/components/ToastContainer/Toast/index.tsx
+++ 06-gobarber-web/src/components/ToastContainer/Toast/index.tsx
@@ -5,12 +5,14 @@ import {
   FiInfo,
   FiXCircle,
 } from 'react-icons/fi';
+
 import { ToastMessage, useToast } from '../../../hooks/toast';
 
 import { Container } from './styles';
 
 interface ToastProps {
   message: ToastMessage;
+  style: object;
 }
 
 const icons = {
@@ -19,7 +21,7 @@ const icons = {
   success: <FiCheckCircle size={24} />,
 };
 
-const Toast: React.FC<ToastProps> = ({ message }) => {
+const Toast: React.FC<ToastProps> = ({ message, style }) => {
   const { removeToast } = useToast();
 
   useEffect(() => {
@@ -33,7 +35,11 @@ const Toast: React.FC<ToastProps> = ({ message }) => {
   }, [removeToast, message.id]);
 
   return (
-    <Container type={message.type} hasDescription={!!message.description}>
+    <Container
+      type={message.type}
+      hasDescription={!!message.description}
+      style={style}
+    >
       {icons[message.type || 'info']}
 
       <div>
